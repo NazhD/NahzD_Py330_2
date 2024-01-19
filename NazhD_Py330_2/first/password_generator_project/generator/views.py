a291c4d9e6065ec907e3f77869e70e5107299746
NahzD_Py330_2 <warmagik@yandex.ru>
2024-01-19 19:06:50 +0500

    first django

---
+++ NazhD_Py330_2/first/password_generator_project/generator/views.py
@@ -0,0 +1,33 @@
+import random
+
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+
+
+def home(request):
+    lst = list(range(6, 15))
+    return render(request, 'generator/home.html', {"lst": lst})
+
+
+def password(request):
+    char = [chr(i) for i in range(97, 123)]
+
+    if request.GET.get('uppercase'):
+        char.extend([chr(i) for i in range(65, 91)])
+
+    if request.GET.get('numbers'):
+        char.extend([chr(i) for i in range(48, 55)])
+
+    if request.GET.get('special'):
+        char.extend([chr(i) for i in range(33, 48)])
+
+    length = int(request.GET.get('length'))
+    psw = ""
+    for i in range(length):
+        psw += random.choice(char)
+    return render(request, "generator/password.html", {'password': psw})
+
+
+def pas_info(request):
+    return render(request, 'generator/info.html')
